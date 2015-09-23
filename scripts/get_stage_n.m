69300e4fd1d117c9d2a9c7bbdf67188b101734b0
David Abrahams <david.abrahams@students.olin.edu>
2015-09-23 02:17:37 -0400

    simplified model. multiple staging.

---
+++ scripts/get_stage_n.m
@@ -0,0 +1,19 @@
+function [stage_n, attached_stage_weight] = get_stage_n(stage_masses, current_fuel_mass)
+
+    temp_mass = current_fuel_mass;
+    stage_n = length(stage_masses) + 1;
+    for i = length(stage_masses):-1:1
+        if temp_mass > 0
+            stage_n = stage_n -1 ;
+            temp_mass = temp_mass - 4 * stage_masses(i);
+        else
+            break;
+        end
+    end
+    
+    if stage_n > length(stage_masses)
+        attached_stage_weight = 0;
+    else
+        attached_stage_weight = sum(stage_masses(stage_n:end));
+
+end
--- scripts/rockets2d.m
+++ scripts/rockets2d.m
@@ -1,102 +1,81 @@
-function res = rockets2d()
+function top_speed = rockets2d(mstage)
 
-    radius = 0.2;
-    Cd = 0.7;
-    m_shell = 100;
-    mfuel_init = 324; %kg
-    v_ex = 10000; %m/s
-    flow = 1; %kg/s
+    time = 1500; % simulation time
+
+    radius = 5; % the radius of the rocket
+    Cd = 0.3; % the drag coefficient of the rocket
+    payload = 1000; % the weight of the payload
+    v_ex = 3000; %exhaust velocity in m/s
+    flow = 122; % rate of fuel consumption kg/s
     p0 = 1.225;  % inital air denisty
-    me = 5.972e24;
+    me = 5.972e24; % mass of earth
     G = 6.67384e-11;
-    r_e = 6371000;
-    A = pi * radius^2;
-    mstage = [12, 12, 12, 12, 12, 12, 12];
-    stagen = 1;
+    r_e = 6378e3; % radius of Earth
+    a = pi * radius^2; % surface area of rocket
+    mfuel_init = sum(mstage) * 4;
+    atm_scale_height = 7000; % km
+    stage_n = 1;
 
-    I = [r_e, 0, sind(90), cosd(90), mfuel_init]; %x,y,vx,vy,mfuel_init
+    Initial_Conidition = [r_e, 0, mfuel_init]; % Stage variables are height, velocity, and fuel
 
-    function dXdt = derivs(t, X)
-        rx = X(1);
-        ry = X(2);
-        vx = X(3);
-        vy = X(4);
-        m_fuel = X(5);
 
-        r_vec = [rx;ry]; % r = xi+yj
-        r_hat = r_vec ./ norm(r_vec);
-        v_vec = [vx; vy]; %v = vx;i + vyi
-        v_hat = v_vec ./ norm(v_vec);
-        h = norm(r_vec) - r_e;
-        p = p0*exp(-h/8000);
+    function res = derivs(t, X)
+
+        r = X(1);
+        v = X(2);
+        m_fuel = X(3);
 
         if m_fuel > 0
-            Ft = v_ex * log(flow / .25*m_fuel + m_shell) * v_hat;
+            Ft = v_ex * flow;
             dmdt = -flow;
         else
             m_fuel = 0;
-            Ft = [0;0];
+            Ft = 0;
             dmdt = 0;
         end
 
-        if stagen == length(mstage)+1
-            m_fuel = 0;
-            attachedstages = 0;
-        else
-            stagen = length(mstage)+1 - ceil(m_fuel/60);
-            attachedstages = sum(mstage(stagen:end));
-            hey =  [stagen, attachedstages, m_fuel]
-        end
+        [stage_n, attached_stage_weight] = get_stage_n(mstage, m_fuel);
+        % For some reason this still flickers.
+%       disp([stage_n, attached_stage_weight, m_fuel]);
+        m_total = payload + m_fuel + attached_stage_weight;
 
-        drxdt = vx;
-        drydt = vy;
-        Fg = (-G * (m_fuel + m_shell + attachedstages)*me/(norm(r_vec))^2)*r_hat;
-        Fd = (-1/2*Cd*p*norm(v_vec)^2*A)*v_hat;
-        a = (Fg + Fd + Ft) / ( m_fuel + m_shell + attachedstages);
-        %dvxdt = (-G*(m+m_shell)*me/(rx)^2*r_hat -1/2*Cd*p*vx^2*A + flow*v_ex)/(m+m_shell);
-        %dvydt = (-G*(m+m_shell)*me/(ry)^2*r_hat -1/2*Cd*p*vy^2*A + flow*v_ex)/(m+m_shell);
+        h = r - r_e;
+        p = p0 * exp(-h / atm_scale_height);
+
+        drdt = v;
+        Fg = -G * m_total * me / r^2;
+        % TODO: CHECK IF THIS IS RIGHT!
+        Fd = -(1/2) * Cd * p * v^2 * a;
+        a = (Fg + Ft) / m_total;
 
 
-
-        dXdt = [drxdt;drydt; a; dmdt];
+        res = [drdt; a; dmdt];
 
     end
 
-    function [value, terminal, direction] = events(time,W)
-        rx = I(:,1);
-        ry = I(:,2);
+    function [value, terminal, direction] = events(~,W)
+        r = W(1);
+        v = W(2);
 
-        r_vec = [rx; ry]; %v = vx;i + vyi
-        h = norm(r_vec) - r_e;
-        p = p0*exp(-h/8000);
+        h = r - r_e;
         value = h;
         direction = -1;
         terminal = 1;
 
     end
 
-    time = 40000;
+
     options = odeset('Events', @events);
-    [T, M] = ode45(@derivs,[0 time], I, options);
+    [T, M] = ode45(@derivs, [0, time], Initial_Conidition, options);
 
-    rx_pos = M(:,1);
-    ry_pos = M(:,2);
-    vx_pos = M(:,3);
-    vy_pos = M(:,4);
-    m_fin = M(:,5);
+    r = M(:,1) - r_e;
+    v = M(:,2);
+    fuel = M(:,3);
 
-    %v_pos(end);
-    %m_fin(end)
-    %vf = v_ex * log((mfuel_init+m_shell)/m_shell);
+    clf;
+    hold on;
+    plot(T, v);
 
-    hold on
-    plot(rx_pos, ry_pos)
-    dom = 0:2*pi/48:2*pi;
-    xp = (r_e*cos(dom));
-    yp = r_e*sin(dom);
-    plot(xp,yp)
-    axis equal
-    %plot(vx_pos, vy_pos)
-    legend('position')
+    top_speed = max(v);
 
 end
