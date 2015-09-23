2121aa2f67515cbe935b2313e2964cfe387a92eb
David Abrahams <david.abrahams@students.olin.edu>
2015-09-22 22:42:06 -0400

    small changes

--- scripts/rockets2d.m
+++ scripts/rockets2d.m
@@ -1,5 +1,5 @@
 function res = rockets2d()
- 
+
     radius = 0.2;
     Cd = 0.7;
     m_shell = 100;
@@ -13,23 +13,23 @@ function res = rockets2d()
     A = pi * radius^2;
     mstage = [12, 12, 12, 12, 12, 12, 12];
     stagen = 1;
-    
+
     I = [r_e, 0, sind(90), cosd(90), mfuel_init]; %x,y,vx,vy,mfuel_init
-   
+
     function dXdt = derivs(t, X)
         rx = X(1);
         ry = X(2);
         vx = X(3);
         vy = X(4);
         m_fuel = X(5);
-        
+
         r_vec = [rx;ry]; % r = xi+yj
         r_hat = r_vec ./ norm(r_vec);
-        v_vec = [vx; vy]; %v = vx;i + vyi 
+        v_vec = [vx; vy]; %v = vx;i + vyi
         v_hat = v_vec ./ norm(v_vec);
         h = norm(r_vec) - r_e;
         p = p0*exp(-h/8000);
-        
+
         if m_fuel > 0
             Ft = v_ex * log(flow / .25*m_fuel + m_shell) * v_hat;
             dmdt = -flow;
@@ -38,7 +38,7 @@ function res = rockets2d()
             Ft = [0;0];
             dmdt = 0;
         end
-               
+
         if stagen == length(mstage)+1
             m_fuel = 0;
             attachedstages = 0;
@@ -47,7 +47,7 @@ function res = rockets2d()
             attachedstages = sum(mstage(stagen:end));
             hey =  [stagen, attachedstages, m_fuel]
         end
-        
+
         drxdt = vx;
         drydt = vy;
         Fg = (-G * (m_fuel + m_shell + attachedstages)*me/(norm(r_vec))^2)*r_hat;
@@ -55,40 +55,40 @@ function res = rockets2d()
         a = (Fg + Fd + Ft) / ( m_fuel + m_shell + attachedstages);
         %dvxdt = (-G*(m+m_shell)*me/(rx)^2*r_hat -1/2*Cd*p*vx^2*A + flow*v_ex)/(m+m_shell);
         %dvydt = (-G*(m+m_shell)*me/(ry)^2*r_hat -1/2*Cd*p*vy^2*A + flow*v_ex)/(m+m_shell);
-     
-        
- 
+
+
+
         dXdt = [drxdt;drydt; a; dmdt];
-        
+
     end
-    
+
     function [value, terminal, direction] = events(time,W)
         rx = I(:,1);
         ry = I(:,2);
-        
-        r_vec = [rx; ry]; %v = vx;i + vyi 
+
+        r_vec = [rx; ry]; %v = vx;i + vyi
         h = norm(r_vec) - r_e;
         p = p0*exp(-h/8000);
         value = h;
         direction = -1;
         terminal = 1;
-        
+
     end
- 
+
     time = 40000;
     options = odeset('Events', @events);
     [T, M] = ode45(@derivs,[0 time], I, options);
-    
+
     rx_pos = M(:,1);
     ry_pos = M(:,2);
     vx_pos = M(:,3);
     vy_pos = M(:,4);
     m_fin = M(:,5);
-    
+
     %v_pos(end);
     %m_fin(end)
     %vf = v_ex * log((mfuel_init+m_shell)/m_shell);
-    
+
     hold on
     plot(rx_pos, ry_pos)
     dom = 0:2*pi/48:2*pi;
@@ -98,5 +98,5 @@ function res = rockets2d()
     axis equal
     %plot(vx_pos, vy_pos)
     legend('position')
-    
+
 end
